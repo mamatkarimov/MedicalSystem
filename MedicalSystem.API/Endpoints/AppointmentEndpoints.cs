d8d520c7d3c687a3542b82d3d47a5f53726163cc
mbegali.pc <mbegali@gmail.com>
2025-05-16 08:38:39 +0500

    12

---
+++ MedicalSystem.API/Endpoints/AppointmentEndpoints.cs
@@ -0,0 +1,38 @@
+﻿using MedicalSystem.Infrastructure.Persistence;
+using static System.Net.WebRequestMethods;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalSystem.API.Endpoints
+{
+    public static class AppointmentEndpoints
+    {
+        public static void MapAuthEndpoints(this WebApplication app)
+        {
+            app.MapGet("/api/appointment/doctor", async (HttpContext http, AppDbContext db) =>
+{
+    var userIdClaim = http.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+    if (userIdClaim == null || !Guid.TryParse(userIdClaim, out var doctorId))
+        return Results.Unauthorized();
+
+    var appointments = await db.Appointments
+        .Where(a => a.DoctorId == doctorId)
+        .Include(a => a.Patient)
+        .OrderBy(a => a.Date)
+        .Select(a => new
+        {
+            a.Id,
+            a.Date,
+            a.Symptoms,
+            a.Status,
+            Patient = a.Patient.Username
+        })
+        .ToListAsync();
+
+    return Results.Ok(appointments);
+})
+.RequireAuthorization("Doctor");
+        }
+    }
+}
---
+++ MedicalSystem.Staff/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MedicalSystem.Staff.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddHttpClient("MedicalApi", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5074");
+});
+
+// Optional: named client
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MedicalApi"));
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
